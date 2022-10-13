f0edf2ae4dec244a5ad5fe35a4f49530c1f81fb7
viruha <maurocastro1@gmail.com>
2022-10-13 18:35:38 -0300

    Update TPTransversal.java

--- src/tptransversal/TPTransversal.java
+++ src/tptransversal/TPTransversal.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tptransversal;
 
-/**
- *
- * @author Mauro
- */
+
 public class TPTransversal {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
