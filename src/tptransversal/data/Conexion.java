70cbb7c17bad5531d9fce2b8da2cfc08a7c7dc60
viruha <maurocastro1@gmail.com>
2022-10-17 17:23:46 -0300

    Conexion y clases Data

---
+++ src/tptransversal/data/Conexion.java
@@ -0,0 +1,43 @@
+
+package tptransversal.data;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conexion {
+    private String url;
+    private String usuario;
+    private String password;
+    private Connection conexion;
+
+    public Conexion(String url, String usuario, String password, Connection conexion) throws ClassNotFoundException {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+        //this.conexion = conexion;
+        //Carga driver mariaDB
+        //Class.forName("org.mariadb.jdbc.Driver");
+    }
+    
+    public Connection conectar(){
+        if(conexion == null)
+        {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(url,usuario,password);
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }             
+        
+    return conexion;
+    }
+       
+   
+
+    
+}
+
